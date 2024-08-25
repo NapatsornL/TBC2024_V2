a572c7b5a7dba2c051cbe0639dfbbdac10e875ef
Kritta <142368906+Krittanai2933@users.noreply.github.com>
2024-08-25 12:17:21 +0700

    Update Section2.jsx

--- src/Section2.jsx
+++ src/Section2.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
-import PurpleBg from '../../../src/assets/BG_P-01.png';
-import AnotherImage1 from '../../tbc2024_v2/src/assets/speakers/speakers-list/1.png';
-import AnotherImage2 from '../../tbc2024_v2/src/assets/speakers/speakers-list/2.png'; 
-import AnotherImage3 from '../../tbc2024_v2/src/assets/speakers/speakers-list/3.png';
-import AnotherImage4 from '../../tbc2024_v2/src/assets/speakers/speakers-list/4.png'; 
-import AnotherImage5 from '../../tbc2024_v2/src/assets/speakers/speakers-list/5.png';
-import AnotherImage6 from '../../tbc2024_v2/src/assets/speakers/speakers-list/6.png';
+import PurpleBg from '../../TBC2024_V2/src/assets/BG_P-01.png';
+import AnotherImage1 from '../../TBC2024_V2/src/assets/speakers/speakers-list/1.png';
+import AnotherImage2 from '../../TBC2024_V2/src/assets/speakers/speakers-list/2.png'; 
+import AnotherImage3 from '../../TBC2024_V2/src/assets/speakers/speakers-list/3.png';
+import AnotherImage4 from '../../TBC2024_V2/src/assets/speakers/speakers-list/4.png'; 
+import AnotherImage5 from '../../TBC2024_V2/src/assets/speakers/speakers-list/5.png';
+import AnotherImage6 from '../../TBC2024_V2/src/assets/speakers/speakers-list/6.png';
 
 const Section2 = () => {
   const [isVisible, setIsVisible] = useState(false);
